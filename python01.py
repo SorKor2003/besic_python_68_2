1d22b737029292f57c7fd4b48f439fa884f98e2e
Thanakorn Saengsee <dsffdf567@gmail.com>
2025-11-22 13:28:55 +0700

    Variable

---
+++ python01.py
@@ -0,0 +1,53 @@
+"""
+#
+#Part: python comment
+#
+"""
+
+
+"""
+v = s / t
+v = speed (m/s)
+s = distance (m)
+t = time (s)
+"""
+
+print("Hello, World!")
+
+"""
+#
+# Part: python Variable
+#
+"""
+x = 5
+y = "5"
+print(x,y)
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variable Naming
+#
+"""
+myvar = "John"
+my_var = "john"
+_my_var = "john"
+myVar = "john"
+MYVAR = "john"
+myvar2 = "john"
+print(myvar, my_var, _my_var, myVar, MYVAR, myvar2)
+
+
+
+
+# Camel Case
+myVariableName = "john"
+# Pascal Case
+MyVariableName = "john"
+# Snake Case
+my_variable_name = "john"
+print(myVariableName, MyVariableName, my_variable_name)
