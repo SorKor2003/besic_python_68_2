05d4683071aa8125b8d119048719b41b3463ffcc
Thanakorn Saengsee <dsffdf567@gmail.com>
2025-11-22 14:33:04 +0700

    collection

---
+++ python03.py
@@ -0,0 +1,48 @@
+"""
+#
+# part: collection of python
+# list, tuple, set, dictionary
+#
+"""
+
+"""
+#
+# list
+#
+"""
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+print(type(thislist))
+print(len(thislist))
+
+"""
+#
+# tuple
+#
+"""
+tuple01 = ("apple", "banana", "cherry", "apple", "cherry")
+print(tuple01)
+print(type(tuple01))
+print(len(tuple01))
+print(tuple01[1])
+
+"""
+#
+# set
+#
+"""
+set01 = {"apple", "banana", "cherry", "apple", "cherry"}
+print(set01)
+print(type(set01))
+print(len(set01))
+
+"""
+#
+# data dictionary
+#
+"""
+dict01 = {"name": "john", "age": 36, "cuntry": "Norway"}
+print(dict01)
+print(dict01["name"])
+print(type(dict01))
+print(len(dict01))
