89a38e6efeae09a3feb709878682c1580a5d4d19
Thanakorn Saengsee <dsffdf567@gmail.com>
2025-11-22 14:09:40 +0700

    Oprators

---
+++ python02.py
@@ -0,0 +1,50 @@
+"""
+#
+# part: Python Oprators
+# Assignment Operators
+#
+"""
+x = 5
+x = x + 3
+x += 3
+
+"""
+#
+# part: Python Oprators
+# Assignment Operators
+#
+"""
+x = 4 + 5
+y = 10 - 2
+z = 3 * 7
+a = 8 / 2
+b = 10 % 3
+
+"""
+#
+# part: Python Oprators
+# Comparison Operators
+#
+"""
+x = 5
+y = 10
+print(x == y)
+print(x != y) 
+print(x > y) 
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+"""
+#
+# part: Python Oprators
+# Logical Operators
+#
+"""
+x = 5
+y = 10
+print((x < 10) and (y > 5))
+print((x < 10) or (y > 5))
+print(not(x < 10))
+print((x < 10) and (y < 5))
+print((x < 10) | (y < 5))
